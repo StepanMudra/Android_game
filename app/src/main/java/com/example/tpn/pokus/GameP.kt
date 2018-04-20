c53b2ed2c5cceabd9bfbb9f84a53238484b50e5d
StepanM <Koule7b@gmail.com>
2018-04-20 22:15:46 +0200

    New class )

---
+++ app/src/main/java/com/example/tpn/pokus/GameP.kt
@@ -0,0 +1,21 @@
+package com.example.tpn.pokus
+
+import android.content.Context
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class GameP(context : Context) : SurfaceView(context), SurfaceHolder.Callback{
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+    }
+
+}
--- app/src/main/java/com/example/tpn/pokus/MainActivity.kt
+++ app/src/main/java/com/example/tpn/pokus/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.tpn.pokus
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+         this.requestWindowFeature(android.view.Window.FEATURE_NO_TITLE)
+        setContentView(GameP(this))
     }
 }
